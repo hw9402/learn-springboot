e6a594142d367ec9d433a9db297a693b77e26f3a
마현우 <mhw35689402@gmail.com>
2023-04-18 22:54:18 +0900

    init

---
+++ Test-Login-Api/src/test/java/com/example/testloginapi/TestLoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testloginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLoginApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
