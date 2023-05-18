957374ae66a513ed117e06e2394994687a213fd1
마현우 <mhw35689402@gmail.com>
2023-05-18 23:54:37 +0900

    add: security configuration

---
+++ Test-Login-Api/src/main/java/com/example/testloginapi/global/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.testloginapi.global.security;
+
+import jakarta.servlet.FilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .rememberMe().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll())
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+}
